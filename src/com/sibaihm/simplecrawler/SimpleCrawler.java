63d231fb8856dbe78356f03df049997cc94c3b5c
sibaihm <sibaihm@gmail.com>
2017-12-17 17:23:44 +0100

    remove the name and license, adjust indents

--- src/com/sibaihm/simplecrawler/SimpleCrawler.java
+++ src/com/sibaihm/simplecrawler/SimpleCrawler.java
@@ -1,20 +1,3 @@
-
-
-/*
- *
- *
- *	Created by Sibai H. Mousa 2017
- *	Version 1.0
- *
- *
- *	Licensed under the GNU General Public License v3.0
- *	You can obtain a copy of the license under:
- *	https://www.gnu.org/licenses/gpl-3.0.en.html
- *
- *
- */
-
-
 package com.sibaihm.simplecrawler;
 
 import java.io.BufferedReader;
@@ -34,49 +17,47 @@ import java.util.regex.Pattern;
 import javax.net.ssl.HttpsURLConnection;
 
 /*
- *	consists of four methods:
- *		(1) crawl which only invokes
- *		(2) traverse which invokes
- *		(3) extractURLsFromHTML and
- *		(4) store
+ * consists of four methods:
+ *   (1) crawl which only invokes
+ *   (2) traverse which invokes
+ *   (3) extractURLsFromHTML and
+ *   (4) store
  */
 
 public class SimpleCrawler {
 	
 	/*
-	 * 	stores the visited urls, so that traverse method can avoid traversing the same url again
+	 * stores the visited urls, so that traverse method can avoid traversing the same url again
 	 */
 	
 	private List<String> visitedURLs = new ArrayList<>();
 	
 	/*
-	 *	stores the domain name in the form of: example.com
+	 * stores the domain name in the form of: example.com
 	 */
 	
 	private String domain;
 	
 	/*
-	 * 	stores the target url which is the root url of the website 
+	 * stores the target url which is the root url of the website 
 	 */
 	
 	private String targetURL;
 	
 	/*
-	 *	stores the number of unique pages on the website
+	 * stores the number of unique pages on the website
 	 */
 	
 	private int numOfUniqueURLs;
 	
 	/*
-	 * 	stores the number of all urls on the website
+	 * stores the number of all urls on the website
 	 */
 	
 	private int numOfAllURLs;
 	
-	
-	
 	/*
-	 * 	the constructor
+	 * the constructor
 	 */
 	
 	public SimpleCrawler(String targetURL) {
@@ -85,8 +66,8 @@ public class SimpleCrawler {
 	}
 	
 	/*
-	 * 	the main method of the class which the user can invoke to start the traversing
-	 * 	it only invokes the traversing method then prints the results on the console
+	 * the main method of the class which the user can invoke to start the traversing
+	 * it only invokes the traversing method then prints the results on the console
 	 */
 	
 	public void crawl() {
@@ -94,37 +75,25 @@ public class SimpleCrawler {
 		traverse(targetURL);
 		
 		/*
-		 *	if you want to print the found urls on the console uncomment these lines
-		 */
-		
-		/*
-		for(String link : visitedURLs) {
-			System.out.println(link);
-		}
-		*/
-		
-		
-		/*
-		 *	printing the found urls on the website
+		 * printing the found urls on the website
 		 */
 		
 		System.out.println("Number of all URLs: " + numOfAllURLs);
 		
 		System.out.println("Number of unique URLs: " + numOfUniqueURLs);
-		
 	}
 	
 	/*
-	 * 	the most important method of the class
-	 * 	traverses a website tree starting with the root url
-	 * 	it invokes the store method, which stores the HTML pages onto the file system
+	 * the most important method of the class
+	 * traverse a website tree starting with the root url
+	 * it invokes the store method, which stores the HTML pages onto the file system
 	 */
 	
 	private void traverse(String targetURL) {
 		
 		/*
-		 *	if the targetURL is already visited
-		 *	return and don't continue 
+		 * if the targetURL is already visited
+		 * return and don't continue 
 		 */
 		
 		if(visitedURLs.contains(targetURL)) {
@@ -132,26 +101,26 @@ public class SimpleCrawler {
 		}
 		
 		/*
-		 * 	otherwise continue traversing
+		 * otherwise continue traversing
 		 */
 		
 		try {
 			
 			/*
-			 * 	adding the url to the visisted urls list
+			 * add the url to the visisted urls list
 			 */
 			
 			visitedURLs.add(targetURL);
 			
 			/*
-			 *	increasing the number of visited urls
-			 *	to print it at the end as a number of unique pages on the website 
+			 * increase the number of visited urls
+			 * to print it at the end as a number of unique pages on the website 
 			 */
 			
 			numOfUniqueURLs++;
 			
 			/*
-			 * 	initializing the url and the http or https requests
+			 * initialize the url and the http or https requests
 			 */
 
 			URL url = new URL(targetURL);
@@ -163,7 +132,7 @@ public class SimpleCrawler {
 			}
 			
 			/*
-			 * 	setting request header elements
+			 * set request header elements
 			 */
 
 			httpConn.setRequestMethod("POST");
@@ -173,7 +142,7 @@ public class SimpleCrawler {
 			httpConn.setDoOutput(true);
 			
 			/*
-			 *	sending the request
+			 * send the request
 			 */
 			
 			OutputStream outputStream = httpConn.getOutputStream();
@@ -183,63 +152,62 @@ public class SimpleCrawler {
 			outputStream.close();
 			
 			/*
-			 * 	getting the response code from the request
+			 * get the response code from the request
 			 */
 			
 			int responseCode = httpConn.getResponseCode();
 			
 			/*
-			 *	if response code is 200
-			 *	storing the HTML page
-			 *	extracting all urls from it
-			 *	continuing traversing the website 
+			 * if response code is 200
+			 * store the HTML page
+			 * extract all urls from it
+			 * continue traversing the website 
 			 */
 			
 			if (responseCode == HttpURLConnection.HTTP_OK) {
 				
 				/*
-				 *	reading the response 
+				 * read the response 
 				 */
 				
-				BufferedReader bufferedReader 	= new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
 				
-				StringBuffer response 		= new StringBuffer();
+				StringBuffer response = new StringBuffer();
 				
 				String inputLine;
 
 				while ((inputLine = bufferedReader.readLine()) != null) {
 					
 					response.append(inputLine);
-					
 				}
 				
 				/*
-				 *	closing the reader 
+				 * close the reader 
 				 */
 				
 				bufferedReader.close();
 				
 				/*
-				 * 	storing the HTML page onto the file system
+				 * store the HTML page onto the file system
 				 */
 				
 				store(response.toString(), targetURL);
 				
 				/*
-				 * 	extracting all urls in the HTML page
+				 * extract all urls in the HTML page
 				 */
 				
 				List<String> urls = extractURLsFromHTML(response.toString());
 				
 				/*
-				 *	storing all found urls in variable,
-				 *	it is not necessary of course, it is only printed in the results
+				 * store all found urls in variable,
+				 * it is not necessary of course, it is only printed in the results
 				 */
 				
 				numOfAllURLs += urls.size();
 				
 				/*
-				 *	iterating through all found urls 
+				 * iterate through all found urls 
 				 */
 				
 				for(String link : urls) {
@@ -247,33 +215,28 @@ public class SimpleCrawler {
 				}
 				
 			/*
-			 *	else if the response code other than 200	
+			 * else if the response code other than 200	
 			 */
 				
 			} else {
 				
 				System.out.println("POST request not worked for: " + targetURL + " response code: " + responseCode);
-				
 			}
 			
 		} catch (MalformedURLException e) {
 			
 			e.printStackTrace();
-			
 		} catch (ProtocolException e) {
 			
 			e.printStackTrace();
-			
 		} catch (IOException e) {
 			
 			e.printStackTrace();
-			
 		}
 	}
 	
-	
 	/*
-	 *	stores the HTML page into a directory on the desktop
+	 * store the HTML page into a directory on the desktop
 	 */
 	
 	private void store(String HTMLPage, String fileName) {
@@ -281,84 +244,81 @@ public class SimpleCrawler {
 		try {
 			
 			/*
-			 *	the path of the directory in which the files will be stored
+			 * the path of the directory in which the files will be stored
 			 */
 			
 			String pathToDir = System.getProperty("user.home") + "\\Desktop\\" + domain + "/";
 			
 			/*
-			 *	creating the directory 
+			 * create the directory 
 			 */
 			
 			new File(pathToDir).mkdir();
 			
 			/*
-			 *	the file path
-			 *	removes the protocol and host name from the file name
-			 *	
+			 * the file path
+			 * remove the protocol and host name from the file name
 			 */
 			
 			fileName = fileName.replace("https://", "").replace("http://",  "").replace(domain, "");
 			
 			/*
-			 *	removing the slash - if existing - from the beginning of the file name
+			 * remove the slash - if exists - from the beginning of the file name
 			 */
 			
 			if(fileName.startsWith("/")) fileName = fileName.substring(1, fileName.length());
 			
 			/*
-			 *	removing the slash - if existing - from the end of the file name
+			 * remove the slash - if exists - from the end of the file name
 			 */
 			
 			if(fileName.endsWith("/")) fileName = fileName.substring(0, fileName.length()-1);
 			
 			/*
-			 *	creating sub-directory if necessary
-			 *	after removing any slashes in the previous statements, if the next condition is true
-			 *	that means there are sub-directories 
+			 * create sub-directory if necessary
+			 * after removing any slashes in the previous statements, if the next condition is true
+			 * that means there are sub-directories 
 			 */
 			
 			if(fileName.contains("/")) {
 				
 				/*
-				 *	splitting the url by the slash 
+				 * split the url by the slash 
 				 */
 				
 				String[] subDir = fileName.split("/");
 				
 				/*
-				 *	taking a copy of the main directory
-				 *	to create sub-directories based on it
-				 *	the new copy will be the main and the other sub-directories will be based on it 
+				 * take a copy of the main directory
+				 * to create sub-directories based on it
+				 * the new copy will be the main and the other sub-directories will be based on it 
 				 */
 				
 				String pathToSubDir = pathToDir;
 				
 				/*
-				 *	looping the array to create sub-directories for all elements except the last one,
-				 *	which is the HTML file 
+				 * loop the array to create sub-directories for all elements except the last one,
+				 * which is the HTML file 
 				 */
 				
 				for(int i=0;i<subDir.length-1;i++) {
 					
 					/*
-					 *	editing the depth of the path for every element in the array 
+					 * edit the depth of the path for every element in the array 
 					 */
 					
 					pathToSubDir += subDir[i] + "/";
 					
 					/*
-					 *	creating the new sub-directory
+					 * create the new sub-directory
 					 */
 
 					new File(pathToSubDir).mkdir();
-					
 				}
-				
 			}
 			
 			/*
-			 *	writing the HTML content into a file on the file system
+			 * write the HTML content into a file on the file system
 			 */
 			
 			FileWriter fileWriter = new FileWriter(new File(pathToDir + fileName + ".html"));
@@ -366,7 +326,7 @@ public class SimpleCrawler {
 			fileWriter.write(HTMLPage);
 			
 			/*
-			 *	closing the file 
+			 * close the file 
 			 */
 			
 			fileWriter.close();
@@ -374,59 +334,57 @@ public class SimpleCrawler {
 		} catch (IOException e) {
 			
 			e.printStackTrace();
-			
 		}
 	}
 	
-	
 	/*
-	 *	extracts the urls from the HTMLPage,
-	 *	returns a list with all the urls in the page
+	 * extract the urls from the HTMLPage,
+	 * return a list with all the urls in the page
 	 */
 	
 	private List<String> extractURLsFromHTML(String HTMLPage) {
 		
 		/*
-		 *	used to store the extracted <a> tags and href attributes 
+		 * used to store the extracted <a> tags and href attributes 
 		 */
 		
 		List<String> urls = new ArrayList<String>();
 		
 		/*
-		 *	pattern used to extract the attributes of <a> tag
+		 * pattern used to extract the attributes of <a> tag
 		 */
 		
 		Pattern patternATag = Pattern.compile("(?i)<a([^>]+)>(.+?)</a>");
 		
 		/*
-		 *	pattern used to extract only the content of href attribute
+		 * pattern used to extract only the content of href attribute
 		 */
 		
 		Pattern patternHref = Pattern.compile("\\s*(?i)href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))");
 		
 		/*
-		 *	matches the htmlPage against the <a> tags
+		 * match the htmlPage against the <a> tags
 		 */
 		
 		Matcher matcherATag = patternATag.matcher(HTMLPage);
 
 	    /*
-	     * 	extracts all <a> tags first, then extracts the href from these <a> tags
-	     * 	the reason is to avoid adding CSS, JS and other links located in the header or footer into the list
-		 *	the first while is for extracting <a> tags
+	     * extract all <a> tags first, then extracts the href from these <a> tags
+	     * the reason is to avoid adding CSS, JS and other links located in the header or footer into the list
+	     * the first while is for extracting <a> tags
 	     */
 		
 	    while (matcherATag.find()) {
 	    	
 	    	/*
-	    	 *	these conditions are to avoid inserting external websites urls
-	    	 *	NOT 100%. There are certainly other cases which are not included here
-	    	 *	but here are the most cases 
+	    	 * these conditions are to avoid inserting external websites urls
+	    	 * NOT 100%. There are certainly other cases which are not included here
+	    	 * but here are the most cases 
 	    	 */
 	    	
 	    	/*
-	    	 *	if the url contains the whole domain name including the protocol
-	    	 *	its more likely that it is a link to some page in the domain and not to external website  
+	    	 * if the url contains the whole domain name including the protocol
+	    	 * its more likely that it is a link to some page in the domain and not to external website  
 	    	 */
 	    	
 	    	if(matcherATag.group(1).contains(targetURL) && !urls.contains(matcherATag.group(1)))
@@ -434,9 +392,9 @@ public class SimpleCrawler {
 	    		urls.add(matcherATag.group(1));
 	    	
 	    	/*
-	    	 * 	if there is no popular domain name in the url
-	    	 * 	its more likely that the url is a relative url, so it is not external url
-	    	 * 	you can add more domain names to the if statement 
+	    	 * if there is no popular domain name in the url
+	    	 * its more likely that the url is a relative url, so it is not external url
+	    	 * you can add more domain names to the if statement 
 	    	 */
 	    	
 	    	if( (!matcherATag.group(1).contains("www") && !matcherATag.group(1).contains("com")
@@ -447,66 +405,63 @@ public class SimpleCrawler {
 	    		urls.add(matcherATag.group(1));
 	    }
 	    
-	    /*
-		 *	matches the urls list against the href attribute
+	        /*
+		 * match the urls list against the href attribute
 		 */
 	    
 	    Matcher matcherHref = patternHref.matcher(urls.toString());
 	    
-	    /*
-		 *	clearing the list to use it again for storing the urls inside href attributes
+	        /*
+		 * clear the list to use it again for storing the urls inside href attributes
 		 */
 	    
 	    urls.clear();
 	    
 	    /*
-	     *	extracting the content of href attribute
-	     *	and editing the content to be in the form of: https://domain.name/blabla
-	     *	then adding it into urls list
+	     * extracting the content of href attribute
+	     * and editing the content to be in the form of: https://domain.name/blabla
+	     * then adding it into urls list
 	     */
 	    
 	    while (matcherHref.find()) {
 	    	
 	    	/*
-	    	 *	storing the url in a variable preparing to edit it 
+	    	 * store the url in a variable preparing to edit it 
 	    	 */
 	    	
 	    	String match = matcherHref.group(1);
 	    	
 	    	/*
-	    	 *	removing any single or double quotes
+	    	 * remove any single or double quotes
 	    	 */
 	    	
 	    	match = match.replace("'", "").replace("\"", "");
 	    	
 	    	/*
-	    	 *	never add pages starting with # or ? or containing the full url 
-	    	 *	because that duplicates the pages
+	    	 * never add pages starting with # or ? or containing the full url 
+	    	 * because that duplicates the pages
 	    	 */
 	    	
 	    	if(match.contains(targetURL) || match.contains("#") || match.contains("?")) continue;
 	    	
 	    	/*
-	    	 *	adding a slash to the beginning if it is missed 
+	    	 * add a slash to the beginning if it is missed 
 	    	 */
 
 	    	if(!match.startsWith("/")) match = "/" + match;
 	    	
 	    	/*
-	    	 *	adds the page into the list if it is not already added 
+	    	 * add the page into the list if it is not already added 
 	    	 */
 
     		if(!urls.contains(targetURL + match)) urls.add(targetURL + match);
-    		
 		}
 	    
 	    /*
-	     *	returning the list of urls 
+	     * return the list of urls 
 	     */
 	    
 	    return urls;
-	    
 	}
-
 }
 
